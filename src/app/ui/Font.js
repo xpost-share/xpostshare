9640bd4782c712a43a320487f7d9ab3a618d3782
Ahmad Thoriq Saputra <saputrathoriq@gmail.com>
2024-08-15 10:04:10 +0700

    feat: add file for fonts

---
+++ src/app/ui/Font.js
@@ -0,0 +1,11 @@
+import {Manuale, Poppins} from "next/font/google";
+
+export const nuale = Manuale({
+    weight: '400',
+    subsets: ["latin"],
+})
+
+export const poppins = Poppins({
+    weight: '400',
+    subsets: ["latin"],
+})
