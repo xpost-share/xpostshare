2b1b7d25f4c24904a1cb08cef2ec5afb34182a37
Ahmad Thoriq Saputra <saputrathoriq@gmail.com>
2024-08-22 01:13:46 +0700

    feat: add Tags section, set price for each sub topics with set price for one price, and make the ui cleaner.

--- src/app/ui/components/nav/UploadDialog.js
+++ src/app/ui/components/nav/UploadDialog.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { TagsInput } from "react-tag-input-component";
 
 import {
   Button,
@@ -20,13 +21,73 @@ import {
 } from "@material-tailwind/react";
 
 import { FaAngleLeft } from "react-icons/fa";
-
 import { IoImages } from "react-icons/io5";
 
-export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
+export default function UploadDialog({
+  mainTitle,
+  mainDesc,
+  subTopics,
+  setSubTopics,
+}) {
   const [activeTab, setActiveTab] = useState("sub");
   const [open, setOpen] = useState(false);
+
   const [coverImage, setCoverImage] = useState(null);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [recievePrice, setRecievePrice] = useState(0);
+  const [buyerPrice, setBuyerPrice] = useState(0);
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+
+  const handleBlur = () => setIsFocused(false);
+
+  const handlePriceChange = (e, id) => {
+    const value = e.target.value;
+
+    if (/^\d*\.?\d*$/.test(value)) {
+      setSubTopics((prev) => {
+        const newSubTopics = { ...prev };
+        newSubTopics[id].price = parseFloat(value);
+        return newSubTopics;
+      });
+
+      const totalPrice = Object.keys(subTopics).reduce(
+        (sum, key) => sum + (subTopics[key].price || 0),
+        0
+      );
+
+      setRecievePrice(totalPrice);
+
+      setBuyerPrice(totalPrice + totalPrice * 0.25);
+    }
+  };
+
+  const handleOnePriceChange = (e, which) => {
+    if (/^\d*\.?\d*$/.test(e.target.value) && e.target.value !== "") {
+      if (which === "recieve") {
+        setRecievePrice(parseFloat(e.target.value));
+        setBuyerPrice(
+          parseFloat(e.target.value) + parseFloat(e.target.value) * 0.25
+        );
+      } else {
+        setBuyerPrice(parseFloat(e.target.value));
+        setRecievePrice(parseFloat(e.target.value) / 1.25);
+      }
+    } else {
+      setRecievePrice(0);
+      setBuyerPrice(0);
+    }
+  };
+
+  const formatPrice = (price) => {
+    if (!price) return "0.00";
+    return price.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  };
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -43,32 +104,73 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
 
   const handleOpen = () => setOpen(!open);
 
-  const data = [
+  const len = Object.keys(subTopics).length;
+
+  const handlePostUpload = () => {
+    delete subTopics.default;
+
+    const data = {
+      title: mainTitle,
+      description: mainDesc,
+      cover: coverImage,
+      tags: selectedTags,
+      subTopics,
+    };
+
+    console.log(data);
+  }
+
+  const sections = [
     {
       label: "Sub Topics",
       value: "sub",
       bodi: (
-        <div className="w-full flex flex-col gap-4 min-h-96 ">
-          <div className="max-h-[20rem] h-[20rem] overflow-y-scroll grid-cols-1 m-3 grid gap-1 ">
+        <div className="w-full flex flex-col gap-4 min-h-96">
+          <div
+            className={`max-h-[20rem] h-[20rem] overflow-y-auto flex flex-col gap-2 items-center ${
+              len === 1 ? "justify-center" : ""
+            }
+          }`}
+          >
             {Object.keys(subTopics).map((key) => {
               const { title } = subTopics[key];
+
+              if (len === 1) {
+                return (
+                  <h1
+                    key="default"
+                    className="text-xl text-black font-bold cursor-pointer hover:text-amber-500/90 duration-300 transition-colors ease-linear"
+                    onClick={handleOpen}
+                  >
+                    Add a sub topic
+                  </h1>
+                );
+              }
               if (key !== "default" || (title && title.trim() !== "")) {
                 return (
                   <div
                     key={key}
-                    className="w-full py-2 px-5 bg-white SubTop flex justify-between gap-3 items-center rounded-md"
+                    className="w-full py-2 px-5 bg-white border-b-2 border-black/50  flex justify-between gap-3 items-center rounded-md"
                   >
-                    <h1 className="text-base text-black w-[90%] line-clamp-2 break-words">
-                      {title}
+                    <h1 className={`text-base font-medium  w-[90%] line-clamp-2 break-words ${!title || title.trim() === "" ? "text-gray-600" : 'text-black'}`}>
+                      {!title || title.trim() === "" ? "Sub Topic " + 1 : title}
                     </h1>
                     <input
                       type="text"
-                      className="py-1.5 px-4 bg-[rgba(255,160,8,0.07)] w-[30%] outline-[#ff9d0079] text-black outline outline-2 rounded-full"
+                      value={
+                        isFocused
+                          ? subTopics[key].price
+                          : formatPrice(subTopics[key].price)
+                      }
+                      onFocus={handleFocus}
+                      onBlur={handleBlur}
+                      onChange={(e) => handlePriceChange(e, key)}
+                      className="py-1.5 overflow-x-auto px-4 bg-[rgba(255,160,8,0.07)] w-[30%] outline-[#ff9d0079] text-black outline outline-2 rounded-full"
                     />
                   </div>
                 );
               }
-              return null; // Return null if the conditions are not met
+              return null;
             })}
           </div>
           <div className="bg-amber-300/10 p-5 rounded-3xl shadow-sm w-full flex items-center justify-evenly">
@@ -78,7 +180,9 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
               </label>
               <input
                 id="price"
+                value={formatPrice(recievePrice)}
                 type="text"
+                disabled={true}
                 className="w-full rounded-full bg-[#FFFFF0] outline outline-2 outline-[#848408] text-sm py-1.5 px-4 text-black"
                 placeholder="Input Price"
               />
@@ -89,6 +193,8 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
               </label>
               <input
                 id="buy"
+                value={formatPrice(buyerPrice)}
+                disabled={true}
                 type="text"
                 className="w-full rounded-full bg-[#FFFFF0] outline outline-2 outline-[#848408] text-sm py-1.5 px-4 text-black"
                 placeholder="Input Price"
@@ -109,10 +215,12 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
                 You Recieved
               </label>
               <input
-                id="price"
                 type="text"
+                value={isFocused ? recievePrice : formatPrice(recievePrice)}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onChange={(e) => handleOnePriceChange(e, "recieve")}
                 className="w-full rounded-full bg-[#FFFFF0] outline outline-2 outline-[#848408] text-sm py-1.5 px-4 text-black"
-                placeholder="Input Price"
               />
             </div>
             <div className="flex flex-col items-center gap-2">
@@ -122,8 +230,11 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
               <input
                 id="buy"
                 type="text"
+                value={isFocused ? buyerPrice : formatPrice(buyerPrice)}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onChange={(e) => handleOnePriceChange(e, "buy")}
                 className="w-full rounded-full bg-[#FFFFF0] outline outline-2 outline-[#848408] text-sm py-1.5 px-4 text-black"
-                placeholder="Input Price"
               />
             </div>
           </div>
@@ -137,9 +248,9 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
         open={open}
         handler={handleOpen}
         size="xxl"
-        className="py-5 px-10"
+        className="py-5 px-10 bg-white/70 backdrop-blur-lg"
       >
-        <DialogHeader className="flex items-start">
+        <DialogHeader className="flex items-start pb-0">
           <button
             onClick={handleOpen}
             className="p-1 border-2 border-black rounded-full hover:opacity-75 transition-opacity ease-in duration-400"
@@ -150,7 +261,7 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
         <DialogBody className="grid grid-cols-2 gap-10">
           <div className="flex flex-col gap-5 items-center mx-[10%]">
             <div
-              onClick={() => document.getElementById('cover').click()}
+              onClick={() => document.getElementById("cover").click()}
               className="w-[30rem] bg-black/60 rounded-lg h-64 flex flex-col items-center justify-center gap-5 cursor-pointer "
             >
               {coverImage ? (
@@ -182,15 +293,16 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
               <p className="w-full font-light leading-5 line-clamp-6 text-justify text-xl text-black">
                 {mainDesc}
               </p>
-              <input
-                type="text"
-                placeholder="Tag"
-                className=" rounded-full bg-[#FFFFF0] outline outline-2 outline-[#848408] text-sm py-1.5 px-4 text-black "
+              <TagsInput
+                value={selectedTags}
+                onChange={setSelectedTags}
+                name="tags"
+                placeHolder="Add a tag"
               />
             </div>
           </div>
           <div className="flex flex-col items-center w-full h-full gap-5">
-            <div className="w-full bg-white/5 shadow-md py-4">
+            <div className="w-full bg-white/5 shadow-md py-4 SubTopBg rounded-md">
               <Tabs value={activeTab}>
                 <TabsHeader
                   className="rounded-none border-b flex justify-center border-blue-gray-50 bg-transparent p-0"
@@ -199,7 +311,7 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
                       "bg-transparent mx-[20%] border-b-4 border-amber-700 shadow-none rounded-none",
                   }}
                 >
-                  {data.map(({ label, value }) => (
+                  {sections.map(({ label, value }) => (
                     <Tab
                       key={value}
                       value={value}
@@ -218,7 +330,7 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
                   ))}
                 </TabsHeader>
                 <TabsBody>
-                  {data.map(({ value, bodi }) => (
+                  {sections.map(({ value, bodi }) => (
                     <TabPanel key={value} value={value}>
                       {bodi}
                     </TabPanel>
@@ -234,6 +346,7 @@ export default function UploadDialog({ mainTitle, mainDesc, subTopics }) {
               rounded={true}
               ripple="light"
               className="w-full"
+              onClick={handlePostUpload}
             >
               Upload
             </Button>
